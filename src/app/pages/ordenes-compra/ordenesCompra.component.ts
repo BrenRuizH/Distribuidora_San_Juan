1239f752779996b420e15f2298a56bcc03fa591c
Jorge Axel Hernandez <20001544@alumnos.utleon.edu.mx>
2024-07-12 20:34:17 -0600

    se agrego remision id en html y en el reporte

--- src/app/pages/ordenes-compra/ordenesCompra.component.ts
+++ src/app/pages/ordenes-compra/ordenesCompra.component.ts
@@ -258,7 +258,7 @@ seleccionarTemporalidad(opcion: string) {
       .subscribe((data) => {
         this.ordenesCompraR = data.items;
 
-        const ordenesHTML = this.ordenesCompraR.map((orden: { folio: any; codigo: any; orden_compra_c: any; fecha_orden: any; fecha_entrega: any; total_pares: any; facturaNo: any; status: any; }) => `
+        const ordenesHTML = this.ordenesCompraR.map((orden: { folio: any; codigo: any; orden_compra_c: any; fecha_orden: any; fecha_entrega: any; total_pares: any; facturaNo: any; remision_id:any; status: any; }) => `
         <tr>
           <td>${orden.folio || ''}</td>
           <td>${orden.codigo || ''}</td>
@@ -266,8 +266,9 @@ seleccionarTemporalidad(opcion: string) {
           <td>${orden.fecha_orden || ''}</td>
           <td>${orden.fecha_entrega || ''}</td>
           <td>${Number(orden.total_pares).toLocaleString() || ''}</td>
+          <td>${orden.remision_id || ''}</td>
           <td>${orden.status || ''}</td>
-          <td>${orden.facturaNo || ''}</td>
+          <td>${orden.facturaNo || ''}</td> 
       `).join('');
 
         const tablaHTML = `
@@ -282,6 +283,7 @@ seleccionarTemporalidad(opcion: string) {
               <th>Fecha de Orden</th>
               <th>Fecha de Entrega</th>
               <th>Total de Pares</th>
+              <th>Remision</th>
               <th>Status</th>
               <th>No. Factura</th>
             </tr>
